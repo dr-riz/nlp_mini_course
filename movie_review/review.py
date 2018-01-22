6e1ce7465d06cb491a531e43457db0f1deaf6560
dr-riz <vizvan@gmail.com>
2018-01-22 14:21:55 -0500

    creat vocab file

--- movie_review/review.py
+++ movie_review/review.py
@@ -1,6 +1,8 @@
+from string import punctuation
+from os import listdir
+from collections import Counter
 from nltk.corpus import stopwords
-import string
- 
+
 # load doc into memory
 def load_doc(filename):
 	# open the file as read only
@@ -10,13 +12,13 @@ def load_doc(filename):
 	# close the file
 	file.close()
 	return text
- 
+
 # turn a doc into clean tokens
 def clean_doc(doc):
 	# split into tokens by white space
 	tokens = doc.split()
 	# remove punctuation from each token
-	table = str.maketrans('', '', string.punctuation)
+	table = str.maketrans('', '', punctuation)
 	tokens = [w.translate(table) for w in tokens]
 	# remove remaining tokens that are not alphabetic
 	tokens = [word for word in tokens if word.isalpha()]
@@ -26,9 +28,58 @@ def clean_doc(doc):
 	# filter out short tokens
 	tokens = [word for word in tokens if len(word) > 1]
 	return tokens
+
+# load doc and add to vocab
+def add_doc_to_vocab(filename, vocab):
+	# load doc
+	doc = load_doc(filename)
+	# clean doc
+	tokens = clean_doc(doc)
+	# update counts
+	vocab.update(tokens)
+
+# load all docs in a directory
+def process_docs(directory, vocab):
+	# walk through all files in the folder
+	for filename in listdir(directory):
+		# skip any reviews in the test set
+		if filename.startswith('cv9'):
+			continue
+		# create the full path of the file to open
+		path = directory + '/' + filename
+		# add doc to vocab
+		add_doc_to_vocab(path, vocab)
+
+# define vocab
+vocab = Counter()
+# add all docs to vocab
+process_docs('txt_sentoken/pos', vocab)
+process_docs('txt_sentoken/neg', vocab)
+# print the size of the vocab
+print(len(vocab))
+# print the top words in the vocab
+print(vocab.most_common(50))
+
+# keep tokens with a min occurrence
+min_occurane = 2
+tokens = [k for k,c in vocab.items() if c >= min_occurane]
+print(len(tokens))
+
+# keep tokens with a min occurrence
+min_occurane = 2
+tokens = [k for k,c in vocab.items() if c >= min_occurane]
+print(len(tokens))
+
+# save list to file
+def save_list(lines, filename):
+	# convert lines to a single blob of text
+	data = '\n'.join(lines)
+	# open file
+	file = open(filename, 'w')
+	# write text
+	file.write(data)
+	# close file
+	file.close()
  
-# load the document
-filename = 'txt_sentoken/pos/cv000_29590.txt'
-text = load_doc(filename)
-tokens = clean_doc(text)
-print(tokens)
+# save tokens to a vocabulary file
+save_list(tokens, 'vocab.txt')
