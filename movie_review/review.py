58a420696626ef9d9541df9f4dd6c7c82d3005e7
dr-riz <vizvan@gmail.com>
2018-01-22 17:50:26 -0500

    complete example

--- movie_review/review.py
+++ movie_review/review.py
@@ -1,7 +1,12 @@
+from numpy import array
 from string import punctuation
 from os import listdir
 from collections import Counter
 from nltk.corpus import stopwords
+from keras.preprocessing.text import Tokenizer
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Dropout
 
 # load doc into memory
 def load_doc(filename):
@@ -40,12 +45,14 @@ def doc_to_line(filename, vocab):
 	return ' '.join(tokens)
 
 # load all docs in a directory
-def process_docs(directory, vocab):
+def process_docs(directory, vocab, is_trian):
 	lines = list()
 	# walk through all files in the folder
 	for filename in listdir(directory):
 		# skip any reviews in the test set
-		if filename.startswith('cv9'):
+		if is_trian and filename.startswith('cv9'):
+			continue
+		if not is_trian and not filename.startswith('cv9'):
 			continue
 		# create the full path of the file to open
 		path = directory + '/' + filename
@@ -61,7 +68,34 @@ vocab = load_doc(vocab_filename)
 vocab = vocab.split()
 vocab = set(vocab)
 # load all training reviews
-positive_lines = process_docs('txt_sentoken/pos', vocab)
-negative_lines = process_docs('txt_sentoken/neg', vocab)
-# summarize what we have
-print(len(positive_lines), len(negative_lines))
+positive_lines = process_docs('txt_sentoken/pos', vocab, True)
+negative_lines = process_docs('txt_sentoken/neg', vocab, True)
+# create the tokenizer
+tokenizer = Tokenizer()
+# fit the tokenizer on the documents
+docs = negative_lines + positive_lines
+tokenizer.fit_on_texts(docs)
+# encode training data set
+Xtrain = tokenizer.texts_to_matrix(docs, mode='freq')
+ytrain = array([0 for _ in range(900)] + [1 for _ in range(900)])
+
+# load all test reviews
+positive_lines = process_docs('txt_sentoken/pos', vocab, False)
+negative_lines = process_docs('txt_sentoken/neg', vocab, False)
+docs = negative_lines + positive_lines
+# encode training data set
+Xtest = tokenizer.texts_to_matrix(docs, mode='freq')
+ytest = array([0 for _ in range(100)] + [1 for _ in range(100)])
+
+n_words = Xtest.shape[1]
+# define network
+model = Sequential()
+model.add(Dense(50, input_shape=(n_words,), activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
+# compile network
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+# fit network
+model.fit(Xtrain, ytrain, epochs=50, verbose=2)
+# evaluate
+loss, acc = model.evaluate(Xtest, ytest, verbose=0)
+print('Test Accuracy: %f' % (acc*100))
