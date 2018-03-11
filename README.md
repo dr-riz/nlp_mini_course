00d9bcfdd2b5c1b2bc1552b7f490344df0dbc0bc
dr-riz <vizvan@gmail.com>
2018-03-11 17:47:08 -0400

    day2: cleaning the text

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # nlp_mini_course
 Credit: Dr. Brownlee at machinelearningmastery.com
+
+### References
+1. How to Clean Text for Machine Learning with Python: https://machinelearningmastery.com/clean-text-machine-learning-python/
+2. How to Develop a Deep Learning Bag-of-Words Model for Predicting Movie Review Sentiment: https://machinelearningmastery.com/deep-learning-bag-of-words-model-sentiment-analysis/
+3. How to Prepare Text Data for Machine Learning with scikit-learn: https://machinelearningmastery.com/prepare-text-data-machine-learning-scikit-learn/ 
--- nlp_mini_course.py
+++ nlp_mini_course.py
@@ -7,16 +7,42 @@ file = open(filename, 'rt')
 text = file.read()
 file.close()
 
+num_of_examples=10
+
 #split into words
 from nltk.tokenize import word_tokenize
 tokens = word_tokenize(text)
+print("number of tokens=" + str(len(tokens)))
+print(tokens[:num_of_examples])
+
+#preprocessing: Filter Out Punctuation
+# remove all tokens that are not alphabetic. diff bw "filter out punctuation" and "extracting isalpha()"?
+words = [word for word in tokens if word.isalpha()]
+print("number of words after removing punctionation=" + str(len(words)))
+print(words[:num_of_examples])
+
+# preprocessing: remove stop words e.g. 'the’, ‘is’, ‘are' from title	
+from nltk.corpus import stopwords
+stop_words = stopwords.words('english')
+#print(stop_words)
+
+words = [w for w in words if not w in stop_words]
+print("number of words after removing stop words=" + str(len(words)))
+print(words[:num_of_examples])	
+
+# preprocessing: stemming of words
+from nltk.stem.porter import PorterStemmer
+porter = PorterStemmer()
+words = [porter.stem(word) for word in words]
+print("number of stemmed words=" + str(len(words)))
+print(words[:num_of_examples])
+
 
 out_file = "tokenized_" + filename 
 file_handler = open(out_file,"w")
 
-for item in tokens:
+for item in words:
   file_handler.write("%s\n" % item)
-
 file_handler.close()
 
 
